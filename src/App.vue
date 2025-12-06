119c2e6fa6e141d540ccae873095d858914b8e48
Zhoushuai01 <xsns1129@qq.com>
2025-12-06 14:41:02 +0800

    加载动画平滑过渡不会出现首次进入链接四个主页面出现闪白情况

--- src/App.vue
+++ src/App.vue
@@ -15,9 +15,19 @@
 <style lang="scss">
   @import "uview-plus/index.scss";
   /*每个页面公共css */
-  page {
+   page {
     width: 100%;
     height: 100%;
-    background: #F8F8F8;
+    background:black;
+    animation: fadeIn 0.5s 
+    ease-in-out;
   }
+  @keyframes fadeIn {
+    from {
+        opacity: 0.3;
+    }
+    to {
+        opacity: 1;
+    }
+}
 </style>
